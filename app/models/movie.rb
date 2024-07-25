631ed981701ef9c18c173dbb6100f778ea28beb9
feruncie <fruncie@chicagobooth.edu>
2024-07-25 20:35:30 +0000

    characters and director

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -16,6 +16,14 @@ class Movie < ApplicationRecord
   validates(:director_id, presence: true)
   validates(:title, uniqueness: true)
 
+  has_many( :characters, class_name: "Character", foreign_key: "character_id")
+  def characters
+    my_id = self.id
+    matching_characters = Character.where({ :movie_id => my_id})
+    return matching_characters
+  end 
+
+  belongs_to (:director)
   def director
     key = self.director_id
 
